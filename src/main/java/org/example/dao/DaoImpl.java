b0660c34e11702d6658816dcc12312584f32dd96
legend <karroumhoussam2020@gmail.com>
2025-02-23 22:13:09 +0000

    create IDAO interface and one of it's implementation

---
+++ src/main/java/org/example/dao/DaoImpl.java
@@ -0,0 +1,12 @@
+package org.example.dao;
+// creer par un developeur
+public class DaoImpl implements IDao {
+
+    @Override
+    public double getData() {
+        // example de donne
+        System.out.println("Version base de donnees");
+        double data = 0.0;
+        return data;
+    }
+}
---
+++ src/main/java/org/example/dao/IDao.java
@@ -0,0 +1,6 @@
+package org.example.dao;
+// creer par un concepteur
+public interface IDao {
+    // abstract method
+    public double getData();
+}

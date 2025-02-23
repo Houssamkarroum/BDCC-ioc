3a6140ec1ef81c9f4b49ee78be101c9ae11c7b3a
legend <karroumhoussam2020@gmail.com>
2025-02-23 22:22:06 +0000

    create IMetier interface and one of it's implementation

---
+++ src/main/java/org/example/metier/MetierImpl.java
@@ -0,0 +1,19 @@
+package org.example.metier;
+
+import org.example.dao.DaoImpl;
+import org.example.dao.IDao;
+
+public class MetierImpl implements IMetier {
+   private IDao dao;
+    @Override
+    public double calculer() {
+        double data = dao.getData();
+        double res = data*10;
+        return res;
+    }
+    // pour injecter dans la var dao un objet d'une class qui implement l'interface IDao
+    // 1ere solution avec setter
+    public void setDao(IDao dao) {
+        this.dao = dao;
+    }
+}
